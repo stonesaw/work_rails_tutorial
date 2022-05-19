e5f4288af70296ff191b08295d560a4addcee788
stonesaw <mail.sou.dev@gmail.com>
2022-05-19 14:12:52 +0900

    finish: task D

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery
+
   private
   def current_cart
     Cart.find(session[:cart_id])
   rescue ActiveRecord::RecordNotFound
     cart = Cart.create
-    session[:cart_id]
+    session[:cart_id] = cart.id
     cart
   end
 end
