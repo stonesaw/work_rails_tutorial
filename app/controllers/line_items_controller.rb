04f3f23ee48da03015f59a76fd03c233ce09f347
tanaka ken <tanaka-ken@netlab.jp>
2022-05-23 17:00:42 +0900

    turbo 対応

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -28,6 +28,7 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to @line_item.cart }
+        format.turbo_stream { render :create, status: :created }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new, status: :unprocessable_entity }
