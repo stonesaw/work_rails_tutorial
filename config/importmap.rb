6cb7687adb72ea0ae2c862b633f0c877f5668498
stonesaw <mail.sou.dev@gmail.com>
2022-05-23 11:00:39 +0900

    install rails-ujs

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,3 +2,4 @@
 
 pin "application", preload: true
 pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.0/dist/jquery.js"
+pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@6.0.5/lib/assets/compiled/rails-ujs.js"
