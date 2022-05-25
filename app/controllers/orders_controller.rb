12c7261d6784234ee3edaf345e716bee58d8d530
stonesaw <mail.sou.dev@gmail.com>
2022-05-25 11:03:26 +0900

    add: order pagination

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,17 @@ class OrdersController < ApplicationController
 
   # GET /orders or /orders.json
   def index
-    @orders = Order.all
+    # @orders = Order.all
+    @orders = Order.paginate(
+      page: params[:page],
+      # order: "created_at desc",
+      per_page: 10
+    )
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @orders }
+    end
   end
 
   # GET /orders/1 or /orders/1.json
